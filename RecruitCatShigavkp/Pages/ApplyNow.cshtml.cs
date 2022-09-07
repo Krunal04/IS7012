9f2f5839eda96da01907e64373558377bb5df6a6
Krunal Shigavan <krunal.shigavan@gmail.com>
2022-09-07 09:19:16 -0400

    Add project files.

---
+++ RecruitCatShigavkp/Pages/ApplyNow.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecruitCatShigavkp.Pages
+{
+    public class ApplyNowModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
